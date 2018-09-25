e8062de295e54a0d641e180c805a47e6ecd7acbe
clarsu02 <clarsu02@gmail.com>
2018-09-25 14:31:03 -0500

    final submission with changes made on both .js files 9/25

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: [
-        { description: 'Walk Dog', isCompleted: true},
-        { description: 'Do Laundry', isCompleted: false},
-        { description: 'Vaccumm House',isCompleted: true},
-      ]
-    };
-  }
-  render() {
-    return (
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+   }
+   render() {
+     return (
       <div className="App">
-      <ul>
-        <ToDo />
-        <ToDo />
-        { this.state.todos.map ( (todo,index)=>
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
-           )}
-         </ul>
-       </div>
+       <ul>
+       { this.state.todos.map( (todo, index) =>
+         <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+       )}
+       </ul>
+     </div>
      );
    }
  }
-    )}
-    </ul>
-    </div>
-    );
-  }
-}
 
-export default App;
+ export default App;
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,28 +4,11 @@ class ToDo extends Component {
   render() {
     return (
       <li>
-      <input type ="checkbox" checked= { this.prop.isCompleted }/>
-      <span>{ this.props.description }</span>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span>{ this.props.description }</span>
       </li>
     );
   }
-
 }
+
 export default ToDo;
-import React { Component } from "react";
-import'.App.css';
-import ToDo from './components/ToDo.js';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      <ul>
-        <ToDo />
-        <ToDo />
-      </ul>
-    </div>
-  );
-  }
-}
-export default App;
